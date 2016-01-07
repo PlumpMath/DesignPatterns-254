bd3fec4acadd07580d202e733464d18185f9b773
Graham Mace <majikdragonuk@yahoo.co.uk>
2016-01-07 11:44:39 +1100

    Added Prototype Pattern

---
+++ PrototypePattern/Domain/TheMatrix.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace PrototypePattern.Domain
+{
+	public class TheMatrix : ICloneable
+	{
+		private bool _hasTakenRedPill;
+		private bool _hasTakenBluePill;
+
+		private int _numberOfTimesAgentSmithEncountered;
+		private int _numberOfPhoneCalls;
+		private int _numberOfBulletsFired;
+
+		public void TakeRedPill()
+		{
+			_hasTakenRedPill = true;
+			_hasTakenBluePill = false;
+		}
+
+		public void TakeBluePill()
+		{
+			_hasTakenBluePill = true;
+			_hasTakenRedPill = false;
+		}
+
+		public void EncounterAgentSmith(int numberOfTimes)
+		{
+			_numberOfTimesAgentSmithEncountered++;
+		}
+
+		public void MakePhoneCall()
+		{
+			_numberOfPhoneCalls++;
+		}
+
+		public void FireBullets(int numberOfBullets)
+		{
+			_numberOfBulletsFired++;
+		}
+
+		public void PrintState()
+		{
+			Console.WriteLine("Has taken red pill? {0}", _hasTakenRedPill);
+			Console.WriteLine("Has taken blue pill? {0}", _hasTakenBluePill);
+
+			Console.WriteLine("Number of times Agent Smith encountered: {0}", _numberOfTimesAgentSmithEncountered);
+			Console.WriteLine("Number of phone calls: {0}", _numberOfPhoneCalls);
+			Console.WriteLine("Number of bullets fired: {0}", _numberOfBulletsFired);
+		}
+
+		public object Clone()
+		{
+			return MemberwiseClone();
+		}
+	}
+}
---
+++ PrototypePattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using PrototypePattern.Domain;
+
+namespace PrototypePattern
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Creating the Matrix...");
+			Console.WriteLine();
+
+			TheMatrix theMatrix = new TheMatrix();
+
+			theMatrix.TakeBluePill();
+			theMatrix.EncounterAgentSmith(5);
+			theMatrix.FireBullets(100);
+			theMatrix.MakePhoneCall();
+			theMatrix.PrintState();
+
+			Console.WriteLine();
+			Console.WriteLine("Cloning the Matrix...");
+			Console.WriteLine();
+			
+			TheMatrix newMatrix = theMatrix.Clone() as TheMatrix;
+
+			if (newMatrix != null)
+			{
+				newMatrix.PrintState();
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
